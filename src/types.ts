0f14866e0c179ef68731004ab6c9f4b84ab89c0b
sudhanshu3430 <127764778+sudhanshu3430@users.noreply.github.com>
2024-03-20 23:09:19 +0530

    adding user profile form feature

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,24 @@ export type User = {
     country: string;
 
 }
+
+export type MenuItem = {
+    _id: string;
+    name: string;
+    price: number;
+
+}
+
+export type Restaurant = {
+    _id: string;
+    user: string;
+    restaurantName: string;
+    city: string;
+    country: string;
+    deliveryPrice: number;
+    cuisines: string[];
+    menuItems: MenuItem[];
+    imageUrl: string;
+    lastUpdated: string;
+
+}
