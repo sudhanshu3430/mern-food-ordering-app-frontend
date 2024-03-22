e2f9b1eae5b2e6f1072b6f68991194b8b862ca4a
sudhanshu3430 <127764778+sudhanshu3430@users.noreply.github.com>
2024-03-22 20:12:09 +0530

    added new features

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -6,6 +6,8 @@ import UserProfilePage from "./pages/UserProfilePage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import ManageRestaurantPage from "./pages/ManageRestaurantPage";
 
+import SearchPage from "./pages/SearchPage";
+
 const AppRoutes = () => {
   return (
     <Routes>
@@ -18,26 +20,34 @@ const AppRoutes = () => {
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route
+        path="/search/:city"
+        element={
+          <Layout showHero={false}>
+            <SearchPage />
+          </Layout>
+        }
+      />
 
-      <Route element={<ProtectedRoute/>}>
-      <Route
-        path="/user-profile"
-        element={
-          <Layout>
-            <UserProfilePage />
-          </Layout>
-        }
-      />
-      <Route
-        path="/manage-restaurant"
-        element={
-          <Layout>
-            <ManageRestaurantPage />
-          </Layout>
-        }
-      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+        <Route
+          path="/manage-restaurant"
+          element={
+            <Layout>
+              <ManageRestaurantPage />
+            </Layout>
+          }
+        />
       </Route>
-      
+
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
